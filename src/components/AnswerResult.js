b605c5ed507caea4350c422693ea33dc99edd2b7
DC Morales <dc.morales37@gmail.com>
2022-03-04 11:23:00 -0800

    Reorganize scss

--- src/components/AnswerResult.js
+++ src/components/AnswerResult.js
@@ -2,16 +2,16 @@ import Question from './Question';
 
 const AnswerResult = ({ question }) => {
   return (
-    <div className="result-screen__answer-result">
-      <div className="result-screen__icon">
+    <div className="answer-result">
+      <div className="answer-result__icon">
         {question.userAnswer === question.correct_answer ? (
-          <div className="result-screen__correct">&#43;</div>
+          <div className="answer-result__correct">&#43;</div>
         ) : (
-          <div className="result-screen__wrong">&minus;</div>
+          <div className="answer-result__wrong">&minus;</div>
         )}
       </div>
 
-      <div className="result-screen__question-container">
+      <div className="answer-result__question-container">
         <Question question={question} />
       </div>
     </div>
