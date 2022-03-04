c5e0a71324703a6ba2c46bc6d383bbdf3826b117
DC Morales <dc.morales37@gmail.com>
2022-03-04 09:35:20 -0800

    Update classNames

--- src/components/AnswerResult.js
+++ src/components/AnswerResult.js
@@ -7,7 +7,7 @@ const AnswerResult = ({ question }) => {
         {question.userAnswer === question.correct_answer ? (
           <div className="result-screen__correct">&#43;</div>
         ) : (
-          <div className="result-screen__not-correct">&minus;</div>
+          <div className="result-screen__wrong">&minus;</div>
         )}
       </div>
 
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,18 +1,17 @@
 import ReactHtmlParser from 'react-html-parser';
 
 const Question = ({ question, isCard }) => {
+  const isCorrect = question.userAnswer === question.correct_answer;
+  const isWrong =
+    question.userAnswer && question.userAnswer !== question.correct_answer;
+
   return (
     <>
       <h3>{question.category}</h3>
       <div
         className={`${isCard ? 'quiz-card__question' : ''} ${
-          question.userAnswer === question.correct_answer
-            ? 'correct'
-            : question.userAnswer &&
-              question.userAnswer !== question.correct_answer
-            ? 'not-correct'
-            : null
-        }`}
+          isCard && isCorrect ? 'quiz-card__correct' : ''
+        } ${isCard && isWrong ? 'quiz-card__wrong' : ''}`}
       >
         {ReactHtmlParser(question.question)}
       </div>
