f70921c5dfc254d8f6971a6d31db43d6fc257471
DC Morales <dc.morales37@gmail.com>
2022-03-04 12:19:16 -0800

    Update className

--- src/components/screens/HomeScreen.js
+++ src/components/screens/HomeScreen.js
@@ -3,10 +3,10 @@ import { Button } from '../common/Button';
 const HomeScreen = ({ currentQuestionIdx, handleBeginClick }) => {
   return (
     currentQuestionIdx === -1 && (
-      <div className="screen-content home">
-        <h1 className="home__header">Welcome to the Trivia Challenge</h1>
+      <div className="screen-content home-screen">
+        <h1 className="home-screen__header">Welcome to the Trivia Challenge</h1>
 
-        <h2 className="home__directions">
+        <h2 className="home-screen__directions">
           You will be presented with 10 True or False questions.
         </h2>
 
